eac68517518a6de5ed8c4fa30c471949693f0bbe
Bihan Chakraborty <bhchakraborty001@gmail.com>
2020-11-16 20:16:04 +0530

    added count triplet using sort and two pointers

--- Arrays/count_triplets.cpp
+++ Arrays/count_triplets.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 // Time complexity: O(n^2)
 
-vector<vector<int>> count_triplets(int *arr, int n) {
+vector<vector<int>> count_triplets_hash(int *arr, int n) {
     vector<vector<int>> res;
     map<int, int> hash;
     int k = n - 1;
@@ -19,6 +19,31 @@ vector<vector<int>> count_triplets(int *arr, int n) {
                 tmp.push_back(arr[j]);
                 tmp.push_back(arr[i] + arr[j]);
                 res.push_back(tmp);
+                tmp.clear();
+            }
+        }
+    }
+    return res;
+}
+
+vector<vector<int>> count_triplets_sort(int *arr, int n) {
+    vector<vector<int>> res;
+    sort(arr, arr + n);
+    for (int k = n - 1; k >= 0; k--) {
+        int i = 0, j = k - 1;
+        while (i < j) {
+            if (arr[i] + arr[j] == arr[k]) {
+                vector<int> tmp;
+                tmp.push_back(arr[i]);
+                tmp.push_back(arr[j]);
+                tmp.push_back(arr[k]);
+                res.push_back(tmp);
+                tmp.clear();
+                i++, j--;
+            } else if (arr[i] + arr[j] < arr[k]) {
+                i++;
+            } else {
+                j--;
             }
         }
     }
@@ -28,7 +53,10 @@ vector<vector<int>> count_triplets(int *arr, int n) {
 int main() {
     int arr[] = {5, 32, 1, 7, 10, 50, 19, 21, 2};
     int n = sizeof(arr) / sizeof(arr[0]);
-    vector<vector<int>> ans = count_triplets(arr, n);
+    vector<vector<int>> ans = count_triplets_hash(arr, n);
+    for (auto x : ans) cout << x[0] << " " << x[1] << " " << x[2] << endl;
+    cout << "-----" << endl;
+    ans = count_triplets_sort(arr, n);
     for (auto x : ans) cout << x[0] << " " << x[1] << " " << x[2] << endl;
     return 0;
 }
