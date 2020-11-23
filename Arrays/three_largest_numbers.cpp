fae9a3d641559af1bfe79d27a50378158e3c318e
Bihan Chakraborty <bhchakraborty001@gmail.com>
2020-11-23 21:09:04 +0530

    added three_largest_numbers

--- Arrays/three_largest_numbers.cpp
+++ Arrays/three_largest_numbers.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 // Finding three largest numbers in an array
 
-// Time complexity: O(n)
+// Time complexity: O(n) | Space complexity: O(1)
 vector<int> three_largest_numbers(vi arr) {
     vector<int> res(3, INT_MIN);
     for (int i = 0; i < arr.size(); i++) {
@@ -26,9 +26,25 @@ vector<int> three_largest_numbers(vi arr) {
     return res;
 }
 
+// Can be used to find K largest numbers instead of only 3
+// Time complexity: O(nlogn) | Space complexity: O(n)
+vector<int> three_largest_numbers_1(vi arr) {
+    vector<int> res(3, INT_MIN);
+    priority_queue<int> pq;
+    for (auto it : arr) pq.push(it);
+    for (int i = 0; i < 3; i++) {
+        res[i] = pq.top();
+        pq.pop();
+    }
+    return res;
+}
+
 int main() {
     vi arr = {16, 17, 4, 3, 5, 2};
     vector<int> ans = three_largest_numbers(arr);
+    for (auto x : ans) cout << x << " ";
+    cout << endl;
+    ans = three_largest_numbers_1(arr);
     for (auto x : ans) cout << x << " ";
     return 0;
 }
